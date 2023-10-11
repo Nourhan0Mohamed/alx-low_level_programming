df6c31092bf84911e81ee64400d5b2669efc7f4f
Nourhan0Mohamed <nourmuhd.com>
2023-10-11 00:20:13 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,20 +3,20 @@
 
 /**
  * main - entry point
- *description: prints _putchar, followed by a new line.
+ * description: prints _putchar, followed by a new line.
  * return: 0
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+ _putchar('_');
+ _putchar('p');
+ _putchar('u');
+ _putchar('t');
+ _putchar('c');
+ _putchar('h');
+ _putchar('a');
+ _putchar('r');
+ _putchar('\n');
+ return (0);
 }
