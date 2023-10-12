67db14840727ba644bc0a9af5d5e7868449206cc
Nourhan0Mohamed <nourmuhd.com>
2023-10-12 04:22:40 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
 int main(void)
 {
 _putchar('_');
@@ -11,5 +16,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return 0;
-}
+
+return (0);
