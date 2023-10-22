ce78b2781e5a058d66d71e5dc03067cd05be3824
Nourhan0Mohamed <nourmuhd.com>
2023-10-22 03:17:39 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number;
+
+/* Print single-digit numbers in base 10 */
+for (number = 0; number < 10; number++)
+{
+putchar(number + '0');
+}
+/* Print a newline character */
+putchar('\n');
+
+return (0);
+}
